305c12d81302a91edaafb0f6e3030a49d21b8b8a
Tung Thanh Le <ttungl@users.noreply.github.com>
2016-12-03 16:48:10 -0600

    Add files via upload

--- tungconfigs/testcase1/runt1.sh
+++ tungconfigs/testcase1/runt1.sh
@@ -2,7 +2,12 @@
 # NOC128/cores16/MC4/freq3.0/L2size512/L2assoc8/pipelinewidth4/L1size32/ROB128/bimod4096/bpred1024/fft/
 #note: first run "chmod a+x file.sh"
 
-m2s --x86-sim detailed --x86-report tungconfigs/testcase1/pipeline_mm_mult_serial.out --mem-report tungconfigs/testcase1/mem_mm_mult_serial.out --x86-config tungconfigs/testcase1/x86_cpuconfig --net-config tungconfigs/testcase1/netconfig --net-max-cycles 1000000 --mem-config tungconfigs/testcase1/memconfig --x86-max-inst 1000 --net-report report_mm_mult_serial_8_netrunt1 ./mm_mult_serial/mm_mult_serial 8 
+m2s --x86-sim detailed --x86-report tungconfigs/testcase1/pipeline_mm_mult_serial.out \
+--mem-report tungconfigs/testcase1/mem_mm_mult_serial.out \
+--x86-config tungconfigs/testcase1/x86_cpuconfig --net-config tungconfigs/testcase1/netconfig \
+--net-max-cycles 1000000 --mem-config tungconfigs/testcase1/memconfig \
+--x86-max-inst 1000 \
+--net-report report_mm_mult_serial_8_netrunt1 ./mm_mult_serial/mm_mult_serial 8 
 
 # Stand-alone Interconnection Network
 # m2s --net-config tungconfigs/testcase1/netconfig --net-sim net0 --net-max-cycles 1000000 --net-report report-net ./mm_mult_serial/mm_mult_serial 8
--- tungconfigs/testcase1/runt13.sh
+++ tungconfigs/testcase1/runt13.sh
@@ -4,4 +4,9 @@
 # m2s --x86-sim detailed --x86-report tungconfigs/testcase1/pipeline.out --mem-report tungconfigs/testcase1/mem.out --x86-config tungconfigs/testcase1/x86_cpuconfig --net-config tungconfigs/testcase1/netconfig --net-max-cycles 2000000 --mem-config tungconfigs/testcase1/memconfig --x86-max-inst 1000 --net-report report-net013 --net-injection-rate 0.2
 
 
-m2s --x86-config tungconfigs/testcase1/x86_cpuconfig --mem-config tungconfigs/testcase1/memconfig --net-config tungconfigs/testcase1/netconfig --net-sim net0 --net-max-cycles 1000000 --net-report report-netrunt13-splash2-ocean-p16 ./m2s-bench-splash2/ocean/ocean.i386 -n130 -p16 -e1e-07 -r20000 -t28800
+m2s --x86-config tungconfigs/testcase1/x86_cpuconfig \
+--mem-config tungconfigs/testcase1/memconfig \
+--net-config tungconfigs/testcase1/netconfig \
+--net-sim net0 --net-max-cycles 1000000 \
+--net-report report-netrunt13-splash2-ocean-p16 \
+./m2s-bench-splash2/ocean/ocean.i386 -n130 -p16 -e1e-07 -r20000 -t28800
